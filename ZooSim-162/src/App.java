af953b4f12d5869b855b8b7f70e7a4e909efa286
andro <90076821+iiandromedaa@users.noreply.github.com>
2023-10-13 12:45:08 -0500

    helper methods, checking empty, array management

--- ZooSim-162/src/App.java
+++ ZooSim-162/src/App.java
@@ -6,85 +6,108 @@ public class App {
     static String[] animalSpecies;
     static String[] animalAges;
     static String[] animalHabitats;
+    static Scanner scanscan = new Scanner(System.in);
     static int zooSize;
 
     public static void main(String[] args) throws Exception {
         System.out.println("=-=-=-= ZooSim =-=-=-=");
         System.out.println("The world's most mediocre digital zoo!");
         System.out.println("1) Blank Zoo \n2) Autogenerate Zoo \n3) Load Zoo from CSV\n4) Exit");
-        Scanner scanscan = new Scanner(System.in);
-        boolean valid = false;
-        boolean open = true;
-        if (scanscan.hasNextInt()) {
-            switch(scanscan.nextInt()) {
+        switch(intLoopScan("", 4, 3)) {
+            case 1:
+                int val = intLoopScan("Enter the size of your zoo.", 0, 1);
+                createZoo(val);
+                break;
+            case 2:
+                int valley = intLoopScan("Enter the size of your zoo.", 0, 1);
+                createZoo(valley);
+                break;
+            case 3:
+                System.out.println("Place Zoo.CSV in directory on the same level as App.java\nPress enter to continue");
+                File csv = new File("./Zoo.csv");
+                break;
+            case 4:
+                scanscan.close();
+                System.exit(0);
+                break;
+        }
+        System.out.println("Welcome to your zoo!");
+        while (true) {
+            switch (intLoopScan("1) Search animal\n2) Add animal\n3) Remove animal\n4) Edit animal\n5) Display full zoo\n6) Save to CSV\n7) Exit", 7, 3)) {
                 case 1:
-                    System.out.println("Enter the size of your zoo:");
-                    while(!valid) {
-                        scanscan.nextLine();
-                        if (scanscan.hasNextInt()) {
-                            int inp = scanscan.nextInt();
-                            if (inp > 0) {
-                                valid = true;
-                                createZoo(inp);
-                            }
-                        }
+                    switch (intLoopScan("Search by\n1) Name\n2) Species\n3) Age\n4) Habitat", 4, 1)) {
+                        case 1:
+                            System.out.println("Enter name");
+                        case 2:
+                            System.out.println("Enter species");
+                        case 3:
+                            System.out.println("Enter age");                             
+                        case 4:
+                            System.out.println("Enter habitat");
                     }
                     break;
                 case 2:
-                    System.out.println("Enter the size of your zoo:");
-                    while(!valid) {
-                        scanscan.nextLine();
-                        if (scanscan.hasNextInt()) {
-                            int inp = scanscan.nextInt();
-                            if (inp > 0) {
-                                valid = true;
-                                createZoo(inp);
-                            }
-                        }
+                    if (checkEmpty() == 0) {
+                        System.out.println("Animal list full, please remove an animal entry before adding a new one.");
+                        break;
+                    } else {
+                        
                     }
-                    break;
                 case 3:
-                    System.out.println("Place Zoo.CSV in directory on the same level as App.java\nPress enter to continue");
-                    File csv = new File("./Zoo.csv");
-
+                    break;
                 case 4:
+                    break;
+                case 5:
+                    break;
+                case 6:
+                    break;
+                case 7:
+                    open = false;
                     scanscan.close();
                     System.exit(0);
                     break;
-                default:
-                    System.out.println("Invalid input!");
-                    break;
-            }
-        } else {
-            System.err.println("Not a valid integer");
-            scanscan.close();
-            System.exit(0);
-        };
-        System.out.println("Welcome to your zoo!");
-        while (open = true) {
-            System.out.println("1) Search animal\n2) Add animal\n3) Remove animal\n4) Edit animal\n5) Display full zoo\n6) Save to CSV\n7) Exit");
-            if (scanscan.hasNextInt()) {
-                switch (scanscan.nextInt()) {
-                    case 1:
-                        break;
-                    case 2:
-                        break;
-                    case 3:
-                        break;
-                    case 4:
-                        break;
-                    case 5:
-                        break;
-                    case 6:
-                        break;
-                    case 7:
-                        break;
-                    default:
-                        break;
-                }
-
             }
         }
+    }
+
+    /**
+     * @param prompt
+     * @param bound
+     * @param mode 
+     * 1) GRE bound
+     * 2) LES bound
+     * 3) LEQ bound AND GRE 0
+     * @return parsed int
+     */
+    public static int intLoopScan(String prompt, int bound, int mode) {
+        System.out.print(prompt);
+        while(true){
+            try {
+                int inty = Integer.parseInt(scanscan.next());
+                if (mode == 1) {
+                    if (inty > bound)
+                        return inty;
+                } else if (mode == 2) {
+                    if (inty < bound)
+                        return inty;
+                } else if (mode == 3) {
+                    if (inty <= bound && inty > 0)
+                        return inty;
+                }
+                System.out.println("Invalid input.\n"+prompt);
+            }
+            catch(NumberFormatException ne) {
+                System.out.println("Invalid input.\n"+prompt);
+            }
+        }
+    }
+
+    public static int checkEmpty() {
+        int count = 0;
+        for (int i = 0; i < animalNames.length; i++)
+            if (animalNames[i].equals(""))
+                count++;
+        return count;
     }
 
     public static void createZoo(int zooSize) {
@@ -94,11 +117,53 @@ public class App {
         animalHabitats = new String[zooSize];
     };
 
-    public static void randZoo(String[] names, String[] species, int[] ages, String[] habitats) {
+    public static void randZoo(String[] names, String[] species, String[] ages, String[] habitats) {
 
     };
 
-    public static void bubbleCorresponding(String[] ref, String[] sort0, String[] sort1, String[] sort2) {
-
+    /**
+     * @deprecated
+     */
+    public static void bubbleCorresponding(String[] names, String[] specs, String[] ages, String[] habs, int sortBy) {
+        {
+            String[] arr, arr2, arr3, arr4;
+            switch (sortBy) {
+                case 1:
+                    arr = names;
+                    arr2 = specs;
+                    arr3 = ages;
+                    arr4 = habs;
+                case 2:
+                    
+                case 3:
+                case 4:
+            }
+            int n = names.length;
+            int i, j;
+            String temp, temp2, temp3, temp4;
+            boolean swapped;
+            for (i = 0; i < n - 1; i++) {
+                swapped = false;
+                for (j = 0; j < n - i - 1; j++) {
+                    if (arr[j] > arr[j + 1]) {
+                        temp = arr[j];
+                        temp2 = arr2[j];
+                        temp3 = arr3[j];
+                        temp4 = arr4[j];
+                        arr[j] = arr[j + 1];
+                        arr[j + 1] = temp;
+                        arr2[j] = arr2[j + 1];
+                        arr2[j + 1] = temp2;
+                        arr3[j] = arr3[j + 1];
+                        arr3[j + 1] = temp3;
+                        arr4[j] = arr4[j + 1];
+                        arr4[j + 1] = temp4;
+                        swapped = true;
+                    }
+                }
+                if (swapped == false)
+                    break;
+            }
+        }
     }
 }
